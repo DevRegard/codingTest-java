0cfb181697e5549617737505c69a1a4252709603
UTR-ADP <utr160626@gmail.com>
2021-07-05 21:24:56 +0900

    plus 3 solutions to [2-4]

--- codingTest_java/src/level_2/Countries_124.java
+++ codingTest_java/src/level_2/Countries_124.java
@@ -18,8 +18,21 @@ public class Countries_124 {
 	
 	// [솔루션 1]
 	public String countries_124_1(int n) {
-		String answer = "";
-		return answer;
+		  String[] numbers = {"4", "1", "2"};
+	      String answer = "";
+	      
+	      int num = n;
+	      
+	      while(num > 0){
+	          int remainder = num % 3;
+	          num /= 3;
+	          
+	          if(remainder == 0) num--;
+	          
+	          answer = numbers[remainder] + answer;
+	      }
+	      
+	      return answer;
 	}
 	
 	
@@ -28,8 +41,14 @@ public class Countries_124 {
 	
 	// [솔루션 2]
 	public String countries_124_2(int n) {
-		String answer = "";
-		return answer;
+		String[] num = {"4","1","2"};
+	      String answer = "";
+
+	      while(n > 0){
+	          answer = num[n % 3] + answer;
+	          n = (n - 1) / 3;
+	      }
+	      return answer;
 	}
 	
 	
@@ -39,7 +58,18 @@ public class Countries_124 {
 	// [솔루션 3]
 	public String countries_124_3(int n) {
 		String answer = "";
-		return answer;
+        int k = 0;
+
+        while (n > 0) {
+            k = n % 3; //나머지  - 자릿수 (낮은자리부터)
+            n = n / 3; //몫        - 다음루프의 피제수
+            if (k == 0) {
+                n = n-1; //자리올림을 안 하는 효과
+                k = 4;
+            }
+            answer = k + answer;
+        }
+        return answer;
 	}
 	
 	
