044cde46523335d3ee960f9c76cbc11dfeea54b0
UTR-ADP <utr160626@gmail.com>
2021-07-06 22:03:56 +0900

    Create 1 class

---
+++ codingTest_java/src/level_2/PlainSquare.java
@@ -0,0 +1,55 @@
+package level_2;
+
+/**
+ * @codingTest <Problems> 멀쩡한 사각형 [2-7]
+ *
+ */
+public class PlainSquare {
+
+	
+	public long plainSquare(int w, int h) {
+        long answer = 1;
+        return answer;
+    }
+	
+	
+	
+	
+	
+	
+	public long plainSquare_1(int w, int h) {
+		long answer = 1;
+		return answer;
+	}
+	
+	
+	
+	
+	
+	
+	public long plainSquare_2(int w, int h) {
+		long answer = 1;
+		return answer;
+	}
+	
+	
+	
+	
+	
+	
+	public long plainSquare_3(int w, int h) {
+		long answer = 1;
+		return answer;
+	}
+	
+	
+	
+	
+	
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
